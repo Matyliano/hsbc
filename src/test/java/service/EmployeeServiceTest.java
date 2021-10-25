719d0430024af42b2877ec586148f26e0e744565
joanna.boszko <joanna.boszko@asseco.pl>
2021-10-25 09:09:23 +0200

    add test for query search method

--- src/test/java/service/EmployeeServiceTest.java
+++ src/test/java/service/EmployeeServiceTest.java
@@ -1,32 +1,51 @@
 package service;
 
+import com.example.hsbc.dto.EmployeeDto;
 import com.example.hsbc.entity.Employee;
 import com.example.hsbc.enums.Grade;
 import com.example.hsbc.exception.CustomException;
 import com.example.hsbc.repository.EmployeeRepository;
 import com.example.hsbc.service.EmployeeService;
 
+import com.example.hsbc.util.EmployeeRequestQuery;
+import com.example.hsbc.util.EmployeeUtils;
+import com.example.hsbc.util.RequestSearchEmployeeCriteria;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 public class EmployeeServiceTest {
 
     @Mock
     private EmployeeRepository employeeRepository;
+    @Mock
+    private EmployeeRequestQuery employeeRequestQuery;
+    @Mock
+    private EmployeeUtils employeeUtils;
+    @Mock
+    private Pageable pageable;
 
 
     @InjectMocks
@@ -103,4 +122,27 @@ public class EmployeeServiceTest {
         assertThat(member.getGrade()).isEqualTo(Grade.CHICKEN);
         assertThat(member.getSalary()).isEqualTo(13000.0);
     }
+
+    @Test
+    public void whenGetEmployeesBySearchCriteriaThenReturnEmployees() {
+        //given
+        List<Employee> collect = Collections.singletonList(e2);
+
+
+        Page<Employee> pagedResponse = new PageImpl<>(collect.subList((int) pageable.getOffset(), collect.size()), pageable, collect.size());
+
+        when(employeeRequestQuery.getEmployeesBySearchCriteria(any(Pageable.class), any())).thenReturn(new ArrayList<>());
+        doReturn(pagedResponse).when(employeeUtils).createPageFromListOfEmployeesEntry(anyList(), any(Pageable.class));
+
+        //when
+        Page<EmployeeDto> employees = employeeService.getEmployeesBySearchCriteria(pageable, RequestSearchEmployeeCriteria.builder()
+                        .name(Collections.singletonList("Baba"))
+                        .surname(Collections.singletonList("Yaga"))
+                        .grade(Collections.singletonList(Grade.CHICKEN))
+                        .salary(Collections.singletonList(13000.00))
+                .build());
+        //then
+        assertThat(employees.getContent().size()).isEqualTo(1);
+    }
+
 }
